70812a75ded2e7145e827aa27a3b704f834be2fc
dpereg06 <dpereg06@estudiantes.unileon.es>
2016-04-06 14:19:39 +0200

    Realizado el segundo día de prácticas

--- Date.java
+++ Date.java
@@ -2,30 +2,28 @@ public class Date{
 
 	private int dia;
 	private int mes;
-	private int año;
+	private int anio;
 
 	public Date(){
 
 		dia = 0;
 		mes = 0;
-		año = 0;
+		anio = 0;
 	}
 
-	public void setDia(int valorDia){
-
-		dia = valorDia;
-		comprobacionDia();
+	public void setDia(int dia){
+			
+		this.dia = dia;
 	}
 
-	public void setMes(int valorMes){
+	public void setMes(int mes){
 
-		mes = valorMes;
-		comprobacionMes();
+		this.mes = mes;
 	}
 
-	public void setAño(int valorAño){
+	public void setAño(int anio){
 
-		año = valorAño;
+		this.anio = anio;
 	}
 
 	public int getDia(){
@@ -38,12 +36,128 @@ public class Date{
 		return mes;
 	}
 
-	public int getAño(){
+	public int getAnio(){
 	
-		return año;
+		return anio;
+	}
+
+	public boolean isSameYear(int anio){
+		boolean sameYear = false;
+		
+		if (this.anio == anio)
+			sameYear = true;
+
+		return sameYear;
+	}
+
+	public boolean isSameMonth(int mes){
+		boolean sameMonth = false;
+		
+		if (this.mes == mes)
+			sameMonth = true;
+
+		return sameMonth;
+	}
+
+	public boolean isSameDay(int dia){
+		boolean sameDay = false;
+		
+		if (this.dia == dia)
+			sameDay = true;
+
+		return sameDay;
+	}
+
+	public String nombreMes(){
+		StringBuffer salida = new StringBuffer();
+
+		switch(mes){
+			case 1:
+			salida.append("Enero");
+			break;
+			case 2:
+			salida.append("Febrero");
+			break;
+			case 3:
+			salida.append("Marzo");
+			break;
+			case 4:
+			salida.append("Abril");
+			break;
+			case 5:
+			salida.append("Mayo");
+			break;
+			case 6:
+			salida.append("Junio");
+			break;
+			case 7:
+			salida.append("Julio");
+			break;
+			case 8:
+			salida.append("Agosto");
+			break;
+			case 9:
+			salida.append("Septiembre");
+			break;
+			case 10:
+			salida.append("Octubre");
+			break;
+			case 11:
+			salida.append("Noviembre");
+			break;
+			case 12:
+			salida.append("Diciembre");
+			break;
+		}
+
+		return salida.toString();
+	}
+
+	public String estacionAnio(){
+
+		StringBuffer salida = new StringBuffer();
+
+		switch(mes){
+			case 1:
+			case 2:
+			case 3:
+			salida.append("Invierno");
+			break;
+			case 4:
+			case 5:
+			case 6:
+			salida.append("Primavera");
+			break;
+			case 7:
+			case 8:
+			case 9:
+			salida.append("Verano");
+			break;
+			case 10:
+			case 11:
+			case 12:
+			salida.append("Invierno");
+			break;
+
+		}
+
+		return salida.toString();
+	}
+
+	public String mesesRestantes(){
+
+		int i;
+		StringBuffer salida = new StringBuffer();
+
+		for(i=mes;i <= 12;i++){
+			salida.append(nombreMes() + "\n");
+		}
+
+		return salida.toString();
 	}
 
 	public boolean comprobacionDia(){
+
 
 		boolean cDia = true;
 
@@ -82,6 +196,7 @@ public class Date{
 
 	public boolean comprobacionMes(){
 
+		
 		boolean cMes = true;
 
 		if (mes < 1 && mes > 12){
@@ -90,11 +205,25 @@ public class Date{
 
 		return cMes;
 	}
-}
 
+	/*public String toString(){
+		StringBuffer salida = new StringBuffer();
+		if (comprobacionDia() == false)
+			salida.append("Día introducido no válido ");
+		if (comprobacionMes == false)
+			salida.append("Mes introducido no válido ");
 		
+		return salida.toString(); 
+	}*/
 
+	public String toString(){
+		StringBuffer salida = new StringBuffer();
 
+		salida.append("Día: " + this.dia + "\nMes: " + this.mes + "\nAño: "+ this.anio);
+
+		return salida.toString();
+	}
+}
 
 
 
